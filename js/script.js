84172dd8a68af906e34558b7de504ffbbfac1d96
PatyCaballero <patycaballero.1995@gmail.com>
2021-09-27 12:16:45 -0400

    desarrollo del proyecto de extensión - Sistema para registrar proyectos de extesiones e investigaciones por parte de docentes y estudiantes de la UNAE

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// INTERFAZ DE PROYECTO
+const targets = document.querySelectorAll('[data-target]')
+const content = document.querySelectorAll('[data-content]')
+
+targets.forEach(target => {
+        target.addEventListener('click', () => {
+            content.forEach(c => {
+                c.classList.remove('active')
+            })
+            const t = document.querySelector(target.dataset.target)
+            t.classList.add('active')
+        })
+    })
+    // ALERTA DE REGISTRADO 
+document.getElementById('boton1').onclick = function() {
+        alert('Has registrado exitosamente');
+    }
+    
